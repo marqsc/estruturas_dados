eade20d1483751f5d2a9f8679ab02dc9b7560a3f
marqsc <clmarques.dev@gmail.com>
2025-11-03 09:43:48 -0300

    Aula do dia 20/10

---
+++ lista/Dinâmica/Listable.java
@@ -0,0 +1,31 @@
+//Lista Estática
+
+/*
+C = void insert (Object data, int LogicIndex) 
+  = void append (Object data) 
+
+R = Object select (int LogicIndex) 
+  = Object[] selectAll()
+
+U = void update (Objeto data, int LogicIndex)
+
+D = Object delete(int LogicIndex)
+  = void clear()
+
+S = sort
+*/
+
+public interface Listable {
+
+  //void insert (Object data, int LogicIndex);
+  void append (Object data) ;
+  Object select (int LogicIndex); 
+  Object[] selectAll();
+  //void update (Object data, int LogicIndex);
+  //Object delete(int LogicIndex);
+  void clear();
+
+  boolean isEmpty();
+  boolean isFull();
+  String print();
+}
